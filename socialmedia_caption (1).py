286148e0d49d274ff36f66aa06726ddbf8012d2c
subornitam26 <subornitam26@gmail.com>
2026-02-02 13:53:14 +0530

    caption generator

---
+++ socialmedia_caption (1).py
@@ -0,0 +1,21 @@
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key = "AIzaSyAO_jKrEfep3LJqoSHzP38GboxeLj7MOHM")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("Social Media Caption Generator")
+
+description = st.text_input("Product/Service description")
+tone = st.selectbox("Choose a tone :",["Witty", "Professional"])
+
+if tone and description:
+        prompt = f"Write 3 social media captions under 30 words,tone:{tone}, about:{description}"
+        
+        with st.spinner("Generating captions with Gemini..."):
+            response = model.generate_content(prompt)
+            caption = response.text
+
+        st.subheader("Generated captions:")
+        st.markdown(caption)
